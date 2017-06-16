9faaab9b1aced3b468f407bfb5ef287beed6c2b3
shahjay00785 <shahjay00785@gmail.com>
2017-06-16 16:35:37 +0530

    category set in menu

--- application/views/User_view/user_menu.php
+++ application/views/User_view/user_menu.php
@@ -1,62 +1,52 @@
 <div class="sub-cate">
-<div class=" top-nav rsidebar span_1_of_left">
- <h3 class="cate">CATEGORIES</h3>
-<ul class="menu">
-<li class="item1"><a href="#">Curabitur sapien<img class="arrow-img" src="images/arrow1.png" alt=""/> </a>
-<ul class="cute">
-<li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-<li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-<li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-</ul>
-</li>
-<li class="item2"><a href="#">Dignissim purus <img class="arrow-img " src="images/arrow1.png" alt=""/></a>
-<ul class="cute">
-<li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-<li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-<li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-</ul>
-</li>
-<li class="item3"><a href="#">Ultrices id du<img class="arrow-img img-arrow" src="images/arrow1.png" alt=""/> </a>
-<ul class="cute">
-<li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-<li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-<li class="subitem3"><a href="product.html">Automatic Fails</a></li>
-</ul>
-</li>
-<li class="item4"><a href="#">Cras iacaus rhone <img class="arrow-img img-left-arrow" src="images/arrow1.png" alt=""/></a>
-<ul class="cute">
-<li class="subitem1"><a href="product.html">Cute Kittens </a></li>
-<li class="subitem2"><a href="product.html">Strange Stuff </a></li>
-<li class="subitem3"><a href="product.html">Automatic Fails </a></li>
-</ul>
-</li>
-<li>
-<ul class="kid-menu">
-<li><a href="product.html">Tempus pretium</a></li>
-<li ><a href="product.html">Dignissim neque</a></li>
-<li ><a href="product.html">Ornared id aliquet</a></li>
-</ul>
-</li>
-<ul class="kid-menu ">
-<li><a href="product.html">Commodo sit</a></li>
-<li ><a href="product.html">Urna ac tortor sc</a></li>
-<li><a href="product.html">Ornared id aliquet</a></li>
-<li><a href="product.html">Urna ac tortor sc</a></li>
-<li ><a href="product.html">Eget nisi laoreet</a></li>
-<li><a href="product.html">Faciisis ornare</a></li>
-<li class="menu-kid-left"><a href="contact.html">Contact us</a></li>
-</ul>
-</ul>
- </div>
-<!--initiate accordion-->
-<div class=" chain-grid menu-chain">
-  <a href="single.html"><img class="img-responsive chain" src="images/wat.jpg" alt=" " /></a>
-  <div class="grid-chain-bottom chain-watch">
-    <span class="actual dolor-left-grid">300$</span>
-    <span class="reducedfrom">500$</span>
-    <h6><a href="single.html">Lorem ipsum dolor</a></h6>
+  <div class=" top-nav rsidebar span_1_of_left">
+
+    <ul class="menu">
+      <h3 class="cate">CATEGORIES</h3>
+      <?php $categories=$this->category_model->category_view();
+      foreach ($categories->result() as $row_category) {
+        ?>
+        <li class="item1"><a href="#"><?php echo $row_category->cat_name; ?><img class="arrow-img" src="images/arrow1.png" alt=""/> </a>
+          <ul class="cute">
+            <li class="subitem1"><a href="product.html">Cute Kittens </a></li>
+            <li class="subitem2"><a href="product.html">Strange Stuff </a></li>
+            <li class="subitem3"><a href="product.html">Automatic Fails </a></li>
+          </ul>
+        </li>
+        <?php
+      }
+      ?>
+
+
+
+
+      <li>
+        <ul class="kid-menu">
+          <li><a href="product.html">Tempus pretium</a></li>
+          <li ><a href="product.html">Dignissim neque</a></li>
+          <li ><a href="product.html">Ornared id aliquet</a></li>
+        </ul>
+      </li>
+      <ul class="kid-menu ">
+        <li><a href="product.html">Commodo sit</a></li>
+        <li ><a href="product.html">Urna ac tortor sc</a></li>
+        <li><a href="product.html">Ornared id aliquet</a></li>
+        <li><a href="product.html">Urna ac tortor sc</a></li>
+        <li ><a href="product.html">Eget nisi laoreet</a></li>
+        <li><a href="product.html">Faciisis ornare</a></li>
+        <li class="menu-kid-left"><a href="contact.html">Contact us</a></li>
+      </ul>
+    </ul>
   </div>
-</div>
-<a class="view-all all-product" href="product.html">VIEW ALL PRODUCTS<span> </span></a>
+  <!--initiate accordion-->
+  <div class=" chain-grid menu-chain">
+    <a href="single.html"><img class="img-responsive chain" src="images/wat.jpg" alt=" " /></a>
+    <div class="grid-chain-bottom chain-watch">
+      <span class="actual dolor-left-grid">300$</span>
+      <span class="reducedfrom">500$</span>
+      <h6><a href="single.html">Lorem ipsum dolor</a></h6>
+    </div>
+  </div>
+  <a class="view-all all-product" href="product.html">VIEW ALL PRODUCTS<span> </span></a>
 </div>
 <div class="clearfix"> </div>
